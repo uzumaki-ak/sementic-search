e7dd3a00c21ec35ad24f76d4743e0c4d8793ba26
uzumaki-ak <anikydv38@gmail.com>
2025-08-29 18:03:52 +0530

    deply errr

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/lib/db";
 import HomeClient from "../components/HomeMaterial";
 
+export const dynamic = "force-dynamic"; // 🚀 Add this line
+
 export default async function HomePage() {
   const movies = await prisma.movie.findMany();
-  
   return <HomeClient movies={movies} />;
 }
